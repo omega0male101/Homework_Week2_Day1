9d3c8bb00861fdd36efdbb029fa8c9bc48f25ecd
Eden <edenstevens101@gmail.com>
2017-03-14 09:17:36 +0000

    Final changes

---
+++ lab_ext.rb
@@ -0,0 +1,10 @@
+class Library
+
+  def initialize (input_title, input_student_name, input_date)
+
+    @team = input_title
+    @student = input_student_name
+    @date = input_date
+  end
+
+end
--- lab_ext_spec.rb
+++ lab_ext_spec.rb
@@ -1,4 +1,19 @@
 require ("minitest/autorun")
 require_relative ("./lab_ext")
 
-class
+class Library
+  
+  def setup
+    @library_account = Library.new { 
+    title: "lord_of_the_rings",
+    rental_details: { 
+     student_name: "Jeff", 
+     date: "01/12/16"
+    }
+  }
+  end
+
+  # def initialize
+
+  # end
+end
